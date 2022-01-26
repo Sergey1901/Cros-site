748ba8779f9ec395160604ae01f4658b5a7a9288
Sergey <dor_serg91@mail.ru>
2022-01-26 19:39:23 +0300

    'crossite'

--- src/App.js
+++ src/App.js
@@ -1,35 +1,56 @@
 
-function  App () {
-    return<div className="wrapper">
-<header>
-    <div className={'headerleft'}>
-        <svg/>
+import React from 'react'
 
-        <div className={'headerinfo'}>
-            <h3>
-                React sneakers
-            </h3>
-            <p>
-                Магазин дорогих кросовок
-            </p>
+function  App () {
+    return<div className="wrapper clear">
+<header className="d-flex justify-between align-center ">
+    <div className='d-flex align-center'>
+        <img width={30} height={30} src="/images.png"/>
+        <div>
+            <h3> React sneakers</h3>
+            <p>Магазин дорогих кросовок</p>
     </div>
     </div>
-    <div></div>
-        <ul>
-            <li>
-                <svg />
+
+        <ul  className="d-flex">
+            <li className="mr-30">
+                <img width={24} height={24} src="/cor.png"/>
                 <span>1205р</span>
                             </li>
-            <li><svg/></li>
+            <li> <img width={18} height={18} src="/user.png"/></li>
         </ul>
-
 </header>
+        <div className="content"></div>
+        <h1>Все кросовки</h1>
     </div>
 
 }
 
 
 export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
